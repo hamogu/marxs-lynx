920dd6cbde80386a3f61765768e9df4897869c34
hamogu <moritz.guenther@gmx.de>
2018-06-06 15:08:48 -0400

    add facet_table function

--- marxslynx/ralfgrating.py
+++ marxslynx/ralfgrating.py
@@ -2,6 +2,7 @@ import numpy as np
 import astropy.units as u
 from scipy.interpolate import RectBivariateSpline
 from astropy.utils.data import get_pkg_data_filename as gpdf
+from astropy.table import Table
 from marxs.base import SimulationSequenceElement
 from marxs.optics import GlobalEnergyFilter
 
@@ -93,9 +94,10 @@ class InterpolateRalfTable(object):
 
 order_selector_Si = InterpolateRalfTable(gpdf('data/gratings/Si_efficiency.dat',
                                               package='marxslynx'))
+order_selector_Si.coating = 'None'
 order_selector_SiPt = InterpolateRalfTable(gpdf('data/gratings/SiPt_efficiency.dat',
                                                 package='marxslynx'))
-
+order_selector_SiPt.coating = 'Pt'
 
 class RalfQualityFactor(SimulationSequenceElement):
     '''Scale probabilites of theoretical curves to measured values.
@@ -129,3 +131,27 @@ def catsupportbars(photons):
 # L1 support: blocks 10 % - better than current 18 %
 # L2 support: blocks 10 % - better than current 19 %
 catsupport = GlobalEnergyFilter(filterfunc=lambda e: 0.9 * 0.9)
+
+
+def facet_table(gas):
+    '''Get table of facet properties
+
+    Parameters
+    ----------
+    gas : `marxs.simulator.Parallel` inststance
+        This can be some kind og container, e.g. a Rowland array
+
+    Returns
+    -------
+    facettab : `astropy.table.Table`
+        Table with facet properties
+    '''
+    facetpos = np.stack(gas.elem_pos)
+    facetang = np.arctan2(facetpos[:, 2, 3], facetpos[:, 1, 3])
+    facetrad = np.sqrt(facetpos[:, 1, 3]**2 + facetpos[:, 2, 3]**2)
+    facetid = [e.id_num for e in gas.elements]
+    facetgrooveang = [e.geometry['groove_angle'] for e in gas.elements]
+    facetcoating = [e.order_selector.coating if hasattr(e.order_selector, 'coating') else '--' for e in gas.elements]
+    return Table([facetid, facetrad, facetang, facetgrooveang, facetcoating],
+                 names=['facet', 'facet_rad', 'facet_ang', 'facet_grooveang',
+                        'facet_coating'])
